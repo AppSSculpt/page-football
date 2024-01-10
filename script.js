f473c4451011ea474f1db360c42d57f5d06d463d
AppSSculpt <146571462+AppSSculpt@users.noreply.github.com>
2024-01-10 20:07:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let pdfDoc;
+
 document.addEventListener('DOMContentLoaded', async function () {
     var qrCodeValue = getParameterValue('qr_code');
     var comprador = getParameterValue('comprador');
@@ -6,7 +8,9 @@ document.addEventListener('DOMContentLoaded', async function () {
         var pdfBytes = await fetch('INGRESSO_PAC.pdf').then(res => res.arrayBuffer());
         var qrCodeImage = await generateQRCodeImage(qrCodeValue);
 
-        var modifiedPdfBytes = await createPDFWithQRCode(pdfBytes, comprador, qrCodeImage);
+        pdfDoc = await PDFLib.PDFDocument.load(pdfBytes);
+
+        var modifiedPdfBytes = await createPDFWithQRCode(comprador, qrCodeImage);
         displayPDF(modifiedPdfBytes);
     } catch (error) {
         console.error('Erro ao criar PDF:', error);
@@ -19,27 +23,22 @@ function getParameterValue(parameterName) {
 }
 
 async function generateQRCodeImage(qrCodeValue) {
-    // Use a biblioteca qrcode.js para gerar a imagem do QR Code
     var qrCode = new QRCode(document.createElement('div'));
     qrCode.makeCode(qrCodeValue);
 
-    // Converta a imagem do QR Code para um formato que pode ser usado no PDF
     var qrCodeImage = await pdfDoc.embedPng(qrCode._el.firstChild.toDataURL());
 
     return qrCodeImage;
 }
 
-async function createPDFWithQRCode(pdfBytes, comprador, qrCodeImage) {
-    var pdfDoc = await PDFLib.PDFDocument.load(pdfBytes);
-    var page = pdfDoc.getPage(0); // Supondo que o QR Code será adicionado à primeira página
+async function createPDFWithQRCode(comprador, qrCodeImage) {
+    var page = pdfDoc.getPage(0);
 
-    // Defina a posição e o tamanho do QR Code na página
     var qrCodeX = 50;
     var qrCodeY = 450;
     var qrCodeWidth = 120;
     var qrCodeHeight = 120;
 
-    // Adicione a imagem do QR Code à página do PDF
     page.drawImage(qrCodeImage, {
         x: qrCodeX,
         y: qrCodeY,
@@ -47,13 +46,11 @@ async function createPDFWithQRCode(pdfBytes, comprador, qrCodeImage) {
         height: qrCodeHeight,
     });
 
-    // Adicione o nome do comprador ao PDF
     var font = await pdfDoc.embedFont(PDFLib.Font.Helvetica);
     var textX = 50;
     var textY = 500;
     page.drawText('Comprador: ' + comprador, { x: textX, y: textY, font });
 
-    // Salve as modificações no PDF
     var modifiedPdfBytes = await pdfDoc.save();
     return modifiedPdfBytes;
 }
@@ -61,22 +58,18 @@ async function createPDFWithQRCode(pdfBytes, comprador, qrCodeImage) {
 function displayPDF(pdfBytes) {
     var pdfBlob = new Blob([pdfBytes], { type: 'application/pdf' });
 
-    // Visualizar PDF
     var viewLink = document.createElement('a');
     viewLink.href = URL.createObjectURL(pdfBlob);
     viewLink.target = '_blank';
     viewLink.innerText = 'Visualizar PDF';
 
-    // Adicionar link de visualização à página
     document.body.appendChild(viewLink);
 
-    // Download PDF
     var downloadLink = document.createElement('a');
     downloadLink.href = URL.createObjectURL(pdfBlob);
     downloadLink.download = 'seu_ingresso.pdf';
     downloadLink.innerText = 'Baixar PDF';
 
-    // Adicionar link de download à página
     document.getElementById('downloadPdf').addEventListener('click', function () {
         document.body.appendChild(downloadLink);
         downloadLink.click();
